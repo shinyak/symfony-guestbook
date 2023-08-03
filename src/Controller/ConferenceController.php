ab4148557438627f7e65cb27e0a676716b5c65a3
skanno <sh_kanno@plusfor.co.jp>
2023-08-03 18:11:19 +0900

    Add the index controller

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -8,11 +8,17 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
-    #[Route('/conference', name: 'app_conference')]
+    #[Route('/', name: 'homepage')]
     public function index(): Response
     {
-        return $this->render('conference/index.html.twig', [
-            'controller_name' => 'ConferenceController',
-        ]);
+        return new Response(
+            <<<EOT
+<html lang="">
+    <body>
+        <img src="/images/under-construction.gif"  alt="under construction" />
+    </body>
+</html>
+EOT
+        );
     }
 }
