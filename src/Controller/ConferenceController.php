e2f8b44ed0ba1a0482959e1dd3c6b33433aaccea
skanno <sh_kanno@plusfor.co.jp>
2023-08-07 18:41:08 +0900

    ユーザーインターフェースを構築する の途中
    「カンファレンスページを作成する」の
    > /conference/ 1 で "最初の" カンファレンスにアクセスして次のエラーを確認してください
    
    まで完了
    
    ```
    symfony composer req "twig/intl-extra:^3"
    ```
    
    を実行するところから

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,23 +2,30 @@
 
 namespace App\Controller;
 
+use App\Entity\Conference;
+use App\Repository\CommentRepository;
+use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class ConferenceController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(Environment $twig, ConferenceRepository $conferenceRepository): Response
     {
-        return new Response(
-            <<<EOT
-<html lang="">
-    <body>
-        <img src="/images/under-construction.gif"  alt="under construction" />
-    </body>
-</html>
-EOT
-        );
+        return new Response($twig->render('conference/index.html.twig', [
+            'conferences' => $conferenceRepository->findAll()
+        ]));
+    }
+
+    #[Route('/conference/{id}', name: 'conference')]
+    public function show(Environment $twig, Conference $conference, CommentRepository $commentRepository): Response
+    {
+        return new Response($twig->render('conference/show.html.twig', [
+            'conference' => $conference,
+            'comments' => $commentRepository->findBy(['conference' => $conference], ['createdAt' => 'DESC']),
+        ]));
     }
 }
