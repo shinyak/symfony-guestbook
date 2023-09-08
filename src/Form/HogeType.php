8a4d3845961e28d6d8c5a65253e17840826f5e62
skanno <sh_kanno@plusfor.co.jp>
2023-09-08 17:37:36 +0900

    add log

--- src/Form/HogeType.php
+++ src/Form/HogeType.php
@@ -12,21 +12,10 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class HogeType extends AbstractType
 {
+    const LOG_FILE = '/Users/skanno/project/etc/guestbook/var/log/form_event.log';
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder->addEventListener(FormEvents::PRE_SUBMIT, function (FormEvent $event) {
-            $inputData = $event->getData();
-            $form = $event->getForm();
-
-            // イベント起動時点ではフォームがbuild済みなので
-            // 入力必須かどうかのオプションを指定しつつ要素を追加し直す
-            $form->remove('fuga');
-            $form->add('fuga', FugaType::class, [
-                'no_submit' => true,
-                'default_requirement' => $inputData['hoge_requires_fuga'] === 'yes',
-            ]);
-        });
-
         $builder
             ->add('hoge_requires_fuga', ChoiceType::class, [
                 'label' => 'fuga必須',
@@ -43,6 +32,44 @@ class HogeType extends AbstractType
             )
             ->add('hoge_submit', SubmitType::class)
         ;
+
+        $builder->addEventListener(FormEvents::PRE_SUBMIT, function (FormEvent $event) {
+            $this->log('PRE_SUBMIT');
+            $inputData = $event->getData();
+            $form = $event->getForm();
+
+            // イベント起動時点ではフォームがbuild済みなので
+            // 入力必須かどうかのオプションを指定しつつ要素を追加し直す
+            $form->remove('fuga');
+            $form->add('fuga', FugaType::class, [
+                'no_submit' => true,
+                'default_requirement' => $inputData['hoge_requires_fuga'] === 'yes',
+            ]);
+        });
+
+
+
+
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+            $this->log('PRE_SET_DATA');
+        });
+        $builder->addEventListener(FormEvents::POST_SET_DATA, function (FormEvent $event) {
+            $this->log('POST_SET_DATA');
+        });
+        $builder->addEventListener(FormEvents::SUBMIT, function (FormEvent $event) {
+            $this->log('SUBMIT');
+        });
+        $builder->addEventListener(FormEvents::POST_SUBMIT, function (FormEvent $event) {
+            $this->log('POST_SUBMIT');
+        });
+    }
+
+    private function log(string $message): void
+    {
+        file_put_contents(
+            self::LOG_FILE,
+            date('H:i:s') . " " . $message . "\n"
+        );
     }
 
     public function configureOptions(OptionsResolver $resolver): void
